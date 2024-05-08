013835ecf9f646a459dfcb907ee22bd5b23dd0ad
muhammadali23 <muhammadali121999@gmail.com>
2024-05-08 22:51:00 +0500

    another problem

--- some_extra_stuf/DSA.js
+++ some_extra_stuf/DSA.js
@@ -33,7 +33,7 @@ function numWithEven(arr) {
 }
 
 let arr1 = [42, 564, 5775, 34, 123, 454, 1, 5, 45, 3556, 23442];
-console.log(numWithEven(arr1));
+// console.log(numWithEven(arr1));
 
 // optimize code
 
@@ -42,4 +42,46 @@ function numWithEven(arr) {
 }
 
 let arr2 = [42, 564, 5775, 34, 123, 454, 1, 5, 45, 3556, 23442];
-console.log(numWithEven(arr2));
+// console.log(numWithEven(arr2));
+
+// palindrome string
+
+// let str = "dad";
+// let len = str.length;
+// let msg = "It is palindrome";
+// for (let i = 0; i < len / 2; i++) {
+//   if (str[i] != str[len - 1 - i]) {
+//     msg = "It is not palindrome";
+//   }
+// }
+// console.log(msg);
+
+// Given two sorted arrays A and B, find the merged sorted array C by merging A and B.
+
+function mergeArray(arr1, arr2) {
+  let arr3 = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      arr3.push(arr1[i]);
+      i++;
+    } else {
+      arr3.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    arr3.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    arr3.push(arr2[j]);
+    j++;
+  }
+  return arr3;
+}
+
+let arr11 = [1, 2, 3, 4, 4];
+let arr12 = [2, 4, 5, 5];
+console.log(mergeArray(arr11, arr12));
